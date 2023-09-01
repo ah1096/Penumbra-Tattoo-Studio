07871a41476fc5c9a2ecb0f0a5d04eaea01792ac
ah1096 <arh.10.96@gmail.com>
2023-09-01 22:10:32 +0000

    add button files

---
+++ penumbra/src/Components/ArtistPageTemplate.js
@@ -0,0 +1,12 @@
+import him from '../images/him.jpg';
+const profilepic = {
+    name: 'him',
+    imageUrl: him,
+};
+
+export const ArtistPageTemplate = () => {
+    return(
+        
+    )
+}
+
--- penumbra/src/Components/Artists.js
+++ penumbra/src/Components/Artists.js
@@ -1,33 +1,73 @@
+import React, { useState } from 'react';
+
+// Define artist data
+const artists = [
+  {
+    name: 'Caleb Ross',
+    imageUrl: 'URL_TO_Caleb_Ross_IMAGE',
+  },
+  {
+    name: 'Ashley Benavides',
+    imageUrl: 'URL_TO_Ashley_Benavides_IMAGE',
+  },
+  {
+    name: 'Leo Flowers',
+    imageUrl: 'URL_TO_Leo_Flowers_IMAGE',
+  },
+  {
+    name: 'Chris Brittain',
+    imageUrl: 'URL_TO_Chris_Brittain_IMAGE',
+  },
+  {
+    name: 'Kris Heartwood',
+    imageUrl: 'URL_TO_Kris_Heartwood_IMAGE',
+  },
+];
+
 export const Artists = () => {
-    return(
-      <div id="artists" class="px-4 py-5 my-5 text-center">
-      <h1 class="display-5 fw-bold">Meet Our Artists</h1>
-      <div class="col-lg-6 mx-auto">
-        <p class="lead mb-4">
-          click on an artist's icon to visit their personal page!
+  const [imageSrc, setImageSrc] = useState('');
+
+  const handleMouseEnter = (imageUrl) => {
+    setImageSrc(imageUrl);
+  };
+
+  const handleMouseLeave = () => {
+    setImageSrc('');
+  };
+
+  return (
+    <div id="artists" className="px-4 py-5 my-5 text-center">
+      <h1 className="display-5 fw-bold">Meet Our Artists</h1>
+      <div className="col-lg-6 mx-auto">
+        <p className="lead mb-4">
+          Click on an artist's icon to visit their personal page!
         </p>
-        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          <button type="button" class="btn btn-primary btn-lg px-4 gap-3">
-            Kris Heartwood
-          </button>
-
-          <button type="button" class="btn btn-primary btn-lg px-4 gap-3">
-            Caleb Ross
-          </button>
-
-          <button type="button" class="btn btn-primary btn-lg px-4 gap-3">
-            Asley Benavides
-          </button>
-
-          <button type="button" class="btn btn-primary btn-lg px-4 gap-3">
-            Leo Flowers
-          </button>
-
-          <button type="button" class="btn btn-primary btn-lg px-4 gap-3">
-            Chris Brittain
-          </button>
+        <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+          {artists.map((artist, index) => (
+            <div
+              key={index}
+              className="artist-container"
+              onMouseEnter={() => handleMouseEnter(artist.imageUrl)}
+              onMouseLeave={handleMouseLeave}
+            >
+              <img
+                src={artist.imageUrl}
+                alt={artist.name}
+                className="artist-image"
+              />
+              <div className="artist-name">{artist.name}</div>
+            </div>
+          ))}
+        </div>
+        <div className="selected-artist">
+          {imageSrc && (
+            <>
+              <h2>Selected Artist:</h2>
+              <img src={imageSrc} alt="Selected Artist" />
+            </>
+          )}
         </div>
       </div>
     </div>
-    )
-}
+  );
+};
