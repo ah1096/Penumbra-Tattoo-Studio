ef3548828aa61d3638f04a00312e2b9dfa721416
ah1096 <arh.10.96@gmail.com>
2023-09-01 20:03:48 +0000

    centered Navbar

--- penumbra/src/Components/NavBar.js
+++ penumbra/src/Components/NavBar.js
@@ -1,16 +1,25 @@
+import him from '../images/him.jpg';
+const logo = {
+    name: 'him',
+    imageUrl: him,
+};
+
+
 export const NavBar = () => {
     return(
 <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Penumbra</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link" href="#home">Home</a>
         <a class="nav-link" href="#aboutus">The Studio</a>
         <a class="nav-link" href="#artists">Artists</a>
+        <a class="navbar-brand" href="#home">
+          <img src={logo.imageUrl} style={{width:70 + 'px'}}/>
+        </a>
+        <a class="nav-link" href="#artists">FAQs</a>
         <a class="nav-link" href="#contactus">Contact Us</a>
       </div>
     </div>
