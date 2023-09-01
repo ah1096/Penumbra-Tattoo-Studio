b6d0c39960536bfb53160939e358448649954e6a
ah1096 <arh.10.96@gmail.com>
2023-09-01 19:14:12 +0000

    break into components

---
+++ penumbra/src/Components/About.js
@@ -0,0 +1,28 @@
+import him from '../images/him.jpg';
+const aboutimage = {
+    name: 'him',
+    imageUrl: him,
+};
+
+
+export const About = () => {
+    return(
+        <div id="aboutus" className="container vh-100 w-100 mx-auto">
+            <div className="row align-items-center vh-100  slide-in-bottom">
+
+                <div id="aboutustext" className="col-8">
+                    <h1>About the Shop</h1>
+                    <p>
+                        We are a tattoo studio located in Lexington, KY -- a XX minute walk from the University of Kentucky campus.
+                        Uhhhhhhhhhhhhh we're cool get tattoos idk
+                        Insert more text here
+                    </p>
+                </div>
+
+                <div id="aboutusimage" className="col-4">
+                    <img className="w-100" src={aboutimage.imageUrl} />
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ penumbra/src/Components/Contact.js
@@ -0,0 +1,20 @@
+export const Contact = () => {
+    return(
+        <div id="contactus" className="container vh-100 w-100 mx-auto">
+        <div className="row align-items-center vh-100  slide-in-bottom">
+
+            <div id="followustext" className="col-8">
+                <h1>Get in touch!</h1>
+                <p>
+                  Follow us on instagram or call us now to book an appointment!
+                </p>
+            </div>
+
+            <div id="shopinstalink" className="col-4">
+              insert shop's instagram profile here
+            </div>
+            
+        </div>
+    </div>
+    )
+}
