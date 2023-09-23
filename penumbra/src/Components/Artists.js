4686d2ea0d7ae8c1ae995484257e21b49a1bfbc4
ah1096 <arh.10.96@gmail.com>
2023-09-23 19:25:07 +0000

    artist buttons working!!

--- penumbra/src/Components/Artists.js
+++ penumbra/src/Components/Artists.js
@@ -1,40 +1,85 @@
 import React, { useState } from 'react';
+import krisbutton1 from '../images/artistbuttons/krisbutton1.png';
+import krisbutton2 from '../images/artistbuttons/krisbutton2.png';
 
-// Define artist data
-const artists = [
-  {
-    name: 'Caleb Ross',
-    imageUrl: 'URL_TO_Caleb_Ross_IMAGE',
-  },
-  {
-    name: 'Ashley Benavides',
-    imageUrl: 'URL_TO_Ashley_Benavides_IMAGE',
-  },
-  {
-    name: 'Leo Flowers',
-    imageUrl: 'URL_TO_Leo_Flowers_IMAGE',
-  },
-  {
-    name: 'Chris Brittain',
-    imageUrl: 'URL_TO_Chris_Brittain_IMAGE',
-  },
-  {
-    name: 'Kris Heartwood',
-    imageUrl: 'URL_TO_Kris_Heartwood_IMAGE',
-  },
-];
+const krisbuttons = {
+  name: 'kris',
+  imageUrl1: krisbutton1,
+  imageUrl2: krisbutton2,
+  imageHeight: '1rem',
+}
+
+import calebbutton1 from '../images/artistbuttons/calebbutton1.png';
+import calebbutton2 from '../images/artistbuttons/calebbutton2.png';
+
+const calebbuttons = {
+  name: 'caleb',
+  imageUrl1: calebbutton1,
+  imageUrl2: calebbutton2,
+  imageHeight: '1rem',
+}
+
+import leobutton1 from '../images/artistbuttons/leobutton1.png';
+import leobutton2 from '../images/artistbuttons/leobutton2.png';
+
+const leobuttons = {
+  name: 'leo',
+  imageUrl1: leobutton1,
+  imageUrl2: leobutton2,
+  imageHeight: '1rem',
+}
+
+// import chrisBbutton1 from '../images/artistbuttons/chrisBbutton1.png';
+// import chrisBbutton2 from '../images/artistbuttons/chrisBbutton2.png';
+
+// const chrisBbuttons = {
+//   name: 'chrisB',
+//   imageUrl1: chrisBbutton1,
+//   imageUrl2: lchrisBbutton2,
+//   imageHeight: '1rem',
+// }
+
+// import ashleybutton1 from '../images/artistbuttons/ashleybutton1.png';
+// import chrisBbutton2 from '../images/artistbuttons/ashleybutton2.png';
+
+// const ashleybuttons = {
+//   name: 'ashley',
+//   imageUrl1: ashleybutton1,
+//   imageUrl2: ashleybutton2,
+//   imageHeight: '1rem',
+// }
+
+function swapButton(buttonId) {
+  const imageElement = document.getElementById(buttonId);
+  const currentSrc = imageElement.getAttribute('src');
+  
+  // Check which button was clicked and toggle the image accordingly
+  if (buttonId === 'krisImage') {
+    if (currentSrc === krisbuttons.imageUrl1) {
+      imageElement.setAttribute('src', krisbuttons.imageUrl2);
+    } else {
+      imageElement.setAttribute('src', krisbuttons.imageUrl1);
+    }
+  } else if (buttonId === 'calebImage') {
+    if (currentSrc === calebbuttons.imageUrl1) {
+      imageElement.setAttribute('src', calebbuttons.imageUrl2);
+    } else {
+      imageElement.setAttribute('src', calebbuttons.imageUrl1);
+    }
+  } else if (buttonId === 'leoImage') {
+    if (currentSrc === leobuttons.imageUrl1) {
+      imageElement.setAttribute('src', leobuttons.imageUrl2);
+    } else {
+      imageElement.setAttribute('src', leobuttons.imageUrl1);
+    }
+  }
+}
+
+
+
 
 export const Artists = () => {
-  const [imageSrc, setImageSrc] = useState('');
-
-  const handleMouseEnter = (imageUrl) => {
-    setImageSrc(imageUrl);
-  };
-
-  const handleMouseLeave = () => {
-    setImageSrc('');
-  };
-
+  
   return (
     <div id="artists" className="px-4 py-5 my-5 text-center">
       <h1 className="display-5 fw-bold">Meet Our Artists</h1>
@@ -43,30 +88,29 @@ export const Artists = () => {
           Click on an artist's icon to visit their personal page!
         </p>
         <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-          {artists.map((artist, index) => (
-            <div
-              key={index}
-              className="artist-container"
-              onMouseEnter={() => handleMouseEnter(artist.imageUrl)}
-              onMouseLeave={handleMouseLeave}
-            >
-              <img
-                src={artist.imageUrl}
-                alt={artist.name}
-                className="artist-image"
-              />
-              <div className="artist-name">{artist.name}</div>
-            </div>
-          ))}
+
+          <button type="button" class="btn">
+            <img id="krisImage" src={krisbuttons.imageUrl1} style={{width:70 + 'px'}} onClick={() => swapButton('krisImage')}></img>
+          </button>
+
+          <button type="button" class="btn">
+            <img id="calebImage" src={calebbuttons.imageUrl1} style={{width:70 + 'px'}} onClick={() => swapButton('calebImage')}></img>
+          </button>
+
+          <button type="button" class="btn">
+            <img id="leoImage" src={leobuttons.imageUrl1} style={{width:70 + 'px'}} onClick={() => swapButton('leoImage')}></img>
+          </button>
+
+          {/* <button type="button" class="btn">
+            <img src={chrisBbuttons.imageUrl1} style={{width:70 + 'px'}}></img>
+          </button>
+
+          <button type="button" class="btn">
+            <img src={ashleybuttons.imageUrl1} style={{width:70 + 'px'}}></img>
+          </button> */}
+
         </div>
-        <div className="selected-artist">
-          {imageSrc && (
-            <>
-              <h2>Selected Artist:</h2>
-              <img src={imageSrc} alt="Selected Artist" />
-            </>
-          )}
-        </div>
+
       </div>
     </div>
   );
