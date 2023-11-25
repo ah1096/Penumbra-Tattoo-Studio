f3879f3f63c59cdcb594c8cafe335af7d70e6bd4
Alyssa <arh.10.96@gmail.com>
2023-11-25 15:47:05 -0500

    fix artistpic spacing

--- penumbra/src/Components/KrisPage.js
+++ penumbra/src/Components/KrisPage.js
@@ -7,49 +7,46 @@ const krisprofilepic = {
 
 export const KrisPage = () => {
     return(
-       <div id="krispage" className="container vh-100 w-100s">
-            <div className="row align-items-center vh-100">
-
-                <div id="aboutustext" className="col-6">
-                    <div>
-                        <img id="artistprofilepic" src={krisprofilepic.imageUrl}></img>
-                        <h1 id="artistname" className="fw-bold">Kris Heartwood</h1>
-                        <h3>artist and owner</h3>
-                    </div>
-                    <p>
-                        My name is Kris. I do art. Give me your money.
-                    </p>
-
-                    <p>
-                        <strong>Text me at 000-000-0000 to book your appointment!</strong>
-                    </p>
-
-                    <div className="row">
-                        <p><strong>What I'm into:</strong></p>
-                        <div className="col-6">
-                            <ul>
-                                <li>nakie art</li>
-                                <li>Bowser cum tatto</li>
-                                <li>ouchy stabby</li>
-                                <li>gay pirate show</li>
-                            </ul>
-                        </div>
-
-                        <div className="col-6">
-                            <ul>
-                                <li>kitties</li>
-                                <li>orange thing</li>
-                                <li>ye</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-
-                <div id="instafeedwidget" class="col-6">
-                   <InstaWidget />
-                </div>
-
-            </div>
+       <div id="krispage" className="container vh-100 w-100">
+  <div className="row align-items-center vh-100">
+    <div id="artisttext" className="col-6">
+      <div id="artistpageheading" className="row align-items-center flex">
+        <img id="artistprofilepic" className="col-4" src={krisprofilepic.imageUrl} alt="Artist Profile"></img>
+        <div className="col">
+          <h1 id="artistname" className="fw-bold">Kris Heartwood</h1>
+          <h3>artist and owner</h3>
         </div>
+      </div>
+      <p>
+        My name is Kris. I do art. Give me your money.
+      </p>
+      <p>
+        <strong>Text me at 000-000-0000 to book your appointment!</strong>
+      </p>
+      <div className="row">
+        <p><strong>What I'm into:</strong></p>
+        <div className="col-6">
+          <ul>
+            <li>nakie art</li>
+            <li>Bowser cum tattoo</li>
+            <li>ouchy stabby</li>
+            <li>gay pirate show</li>
+          </ul>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>kitties</li>
+            <li>orange thing</li>
+            <li>ye</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div id="instafeedwidget" className="col-6">
+      <InstaWidget />
+    </div>
+  </div>
+</div>
     )
 }
